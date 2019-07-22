6219397e9e983ec1e16b8cce7f857ede2321a058
heardMan <markyheard@gmail.com>
2019-07-21 18:06:07 -0700

    feat: added Player Constructor, Player Update function, allEnemies Array, player instance,  and established basic functionality.
     Enemy setY Method
     This method sets the height of the enemy
     based on an array of predetermined heights that correspond to each row
     This method is called at instantiation to set initial height
     and then each time a player leaves the screen so that each transit is more unique
    
     Enemy setSpeed Method
     This method sets the speed of the enemy to a random canvas pixel value between 2 and 5
     this method is called each time an enemy leaves the screen
     This method is called at instantiation to set initial speed
     and then each time a player leaves the screen so that each transit is more unique
    
     Enemy Update Method
     This method receives an unvisersal time parameter from the engine
     this time parameter keeps speed consistent across all devices.
     This method works by first checking to see if a enemy is still with in range (on screen)
     if they are it multiplies the time constant by the enemy's speed factor to generate a new x position
     if not it resets their speed factor, y position, and x position
    
     Player Constructor
     This is the main definition of the Player instance.
     All Player specific attributes and methods should be attached to this Prototype.
    
     Player Render Method
     TODO: This is taken directly from the enemy render method using protypal inheritance.
    
     Player Update Method
     This is the actual methods used to update the player position
     Sanitized and formatted data is passed from the handle input method
     to the player update method for actuated adjustment of player attributes
    
     Player Handle Input Method
     This method checks to make sure that the input submitted meets necessary criteria:
     1. input is a string
     2. string value is a direction
     then the method sanitizes the input by using javascript string interpolation to double ensure
     that input will be received properly
    
     allEnemies Array
     This is the container for all the Enemy instances
    
     player Instance
     this is the actual player instance that passed to the game engine

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
-//  TO DO:
-// 
-//  - COMPLETE ENEMY CLASS 
-//  - CREATE PLAYER CLASS
-
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //starting speed for enemy
+    this.speed = this.setSpeed();
+    //starting x position for enemy
+    this.x =  -115;
+    //starting  position for enemy
+    this.y = this.setY();
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    if (this.x > 505) {
+        //if the enemy is leaving the screen reset enemy height and speed
+        this.y = this.setY();
+        this.speed = this.setSpeed();
+        this.x = -115;
+    } else {
+        // progress enemy in positive x direction using universal time factor
+        this.x  += this.speed * dt;
+        
+    }
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -26,16 +38,76 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.setY = function() {
+   return [62, 145, 227][Math.floor(Math.random() * 3)];
+};
+
+Enemy.prototype.setSpeed = function() {
+    return Math.floor(Math.random() * 250) + 120;
+ };
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+const Player = function() {
+    //starting x position for player
+    this.x = 200;
+    //starting y position for player
+    this.y = 385;
+    //image for player
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(input) {
+     //if left key is pushed within a valid x range move player left
+     if(input === 'left' && this.x > 49) this.x -= 100
+     //if right key is pushed within a valid x range move player right
+     else if(input === 'right' && this.x <= 350) this.x += 100
+     //if up key is pushed within a valid x range move player up
+     else if (input === 'up' && this.y > 99) this.y -= 85
+     //if up key is pushed outside a valid x range reset player position to lowest position
+     else if (input === 'up' && this.y <= 99) console.log('you win'); //run win sequence
+     //if down key is pushed within a valid x range move player down
+     else if(input === 'down' && this.y <= 350) this.y += 85
+      
+     else  return 'No Valid Input Received';
+    
+};
+
+Player.prototype.render = function() {
+    //TODO: borrow this directly from the enemy constructor to save memory
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(input) {
+    //console.log(input);
+    
+    if (enemy1.x === player.x) console.log('collision enemy1');
+    if (enemy2.x === player.x && enemy2.y === player.y) console.log('collision enemy2');
+    if (
+        input === 'up' ||
+        input === 'down' ||
+        input === 'left' || 
+        input === 'right'
+    )
+    {
+        this.update(`${input}`);
+    }
+    else console.log('please enter a valid command');
+
+    
+   
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+const enemy3 = new Enemy();
+const allEnemies = [enemy1, enemy2];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -46,6 +118,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+    //console.log(allowedKeys[e.keyCode]);
+    
     player.handleInput(allowedKeys[e.keyCode]);
 });
